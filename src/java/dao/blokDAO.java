408b6eca6e16b4a7b518325434a3d5d7c930eea9
nabilo <nabilo@DESKTOP-EVSNMPT>
2020-06-03 02:21:53 +0300

    DAO bloks sinifi crud islemleri findALL methodu gostermek icin  tamamlanmasi

---
+++ src/java/dao/blokDAO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.bloks;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import util.DBConnection;
+
+/**
+ *
+ * @author moj
+ */
+public class blokDAO {
+
+    private DBConnection db;
+    private Connection c;
+    private managerDAO managerdao;
+
+    public managerDAO getManagerdao() {
+        if (this.managerdao == null) {
+            this.managerdao = new managerDAO();
+        }
+        return managerdao;
+    }
+
+    public DBConnection getDb() {
+        if (this.db == null) {
+            this.db = new DBConnection();
+        }
+        return db;
+    }
+
+    public Connection getC() {
+        if (this.c == null) {
+            this.c = this.getDb().connect();
+        }
+        return c;
+    }
+
+    public List<bloks> findAll() {
+        List<bloks> bloklist = new ArrayList();
+        try {
+
+            PreparedStatement pst = this.getC().prepareStatement("select * from bloks");
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                bloks tmp = new bloks();
+                tmp.setId_blok(rs.getLong("id_blok"));
+                tmp.setManager(this.getManagerdao().find(rs.getInt("id_manager")));
+                tmp.setBlok_name(rs.getString("blok_name"));
+                tmp.setLevel_no(rs.getInt("level_no"));
+                tmp.setHow_many_KM(rs.getInt("how_many_KM"));
+                tmp.setBlok_type(rs.getString("blok_type"));
+                tmp.setHow_many_home(rs.getInt("how_many_home"));
+                bloklist.add(tmp);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+
+        }
+        return bloklist;
+
+    }
+
+    /* public void insert(bloks blok) {
+        try {
+            Statement st = getC().createStatement();
+            st.executeUpdate( "insert into bloks(blok_name) values('"+blok.getBlok_name()+"')");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+
+        }
+    }*/
+}
