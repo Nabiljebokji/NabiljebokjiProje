80b59f5496109c3f5d9d941e25d257f6d6908520
NOUR KAYALI <>
2020-06-02 21:24:37 +0300

    validaitonBean sınıfı Tamamlandi ,kendi xhtml sayfalarında bazı yerlerde kullanıldı...

---
+++ src/java/controller/ValidationBean.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Nour
+ */
+@Named
+@SessionScoped
+public class ValidationBean implements Serializable {
+
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        String str = value.toString();
+        if (str.length() < 2) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "summary", "en az 3 "));
+        }
+        if (str.length() > 5) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "summary", "en fazla 5"));
+        }
+    }
+}
